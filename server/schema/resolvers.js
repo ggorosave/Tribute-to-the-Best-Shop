551361cd315f32cd5f610b375974be691a63c98d
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-13 19:32:20 -0700

    Added categories query to resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -4,7 +4,11 @@ const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
-
+    Query: {
+        categories: async () => {
+            return await Category.find();
+        },
+    }
 };
 
 module.exports = resolvers;
