e9e8619bda5a01add525ef563e622d3202bded36
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-16 17:49:06 -0700

    Added apolloprovider and browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const App = () => {
 
+  const router = createBrowserRouter([
+    { 
+      path: '/',
+      element: <><h1>Hello World</h1></>,
+    }
+  ])
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <ApolloProvider>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   )
 };
 
