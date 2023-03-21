a3a3559ebe9f5fbeb14ffad70374e416764611de
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-21 11:33:28 -0700

    Added checkout query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,3 +15,11 @@ export const QUERY_PRODUCTS = gql`
     }
   }
 `;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
