4e37901fa4e7fdfe2751d7641611de63153338df
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-20 17:31:15 -0700

    Added productsSlice boilerplate

---
+++ client/src/utils/productsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const productsSlice = createSlice({
+    name: 'products',
+    initialState: {
+        list: []
+    },
+    reducers: {
+
+    }
+});
+
+export default productsSlice.reducer;
