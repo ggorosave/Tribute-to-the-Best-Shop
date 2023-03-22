4b663c23834cc7e866cf479658c3f053b95982f6
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-22 10:09:02 -0700

    Added addUser to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -28,3 +28,24 @@ export const ADD_ORDER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
