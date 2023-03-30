11974d9f6f9ad5369de7ee57f5de829124f3c3a3
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-30 15:12:00 -0700

    Added color palette to theme

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,20 @@
 import Auth from "../../utils/auth";
-import { Box, Flex, Text } from '@chakra-ui/react'
+import {
+    Box,
+    Flex,
+    Text,
+    Link,
+    UnorderedList,
+    ListItem,
+    Heading
+} from '@chakra-ui/react';
+import { Link as RouteLink } from 'react-router-dom';
 
 const Nav = () => {
 
     return (
-        <Flex as='header' w='full'>
-            <Text>Yes</Text>
+        <Flex as='header' w='full' bg='primary.700'>
+            <Text color='gray.50'>Yes</Text>
         </Flex>
     )
 
