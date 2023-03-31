27fb474f27771a6efdb4a5e33ae6daeb17c5ca8c
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-31 15:37:46 -0700

    Added function to filter products to ProductList

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { updateProducts } from "../../utils/reducers/productsSlice";
+import { selectProducts, updateProducts } from "../../utils/reducers/productsSlice";
 import { selectCurrentCategory } from "../../utils/reducers/categorySlice";
 import { useQuery } from "@apollo/client";
 import { QUERY_PRODUCTS } from "../../utils/queries";
@@ -12,23 +12,36 @@ import { Box } from "@chakra-ui/react";
 
 const ProductList = () => {
 
-    // Grabs current category using a selector (Redux)
+    // Grabs current category from state using a selector (Redux)
     const currentCategory = useSelector(selectCurrentCategory);
+    // Grabs products from state using a selector (Redux)
+    const products = useSelector(selectProducts);
     const dispatch = useDispatch();
     
     const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-    if (data) {
-        dispatch(updateProducts(data.products));
-        data.products.forEach((product) => {
-            idbPromise('products', 'put', product);
-        });
-    } else if (!loading) {
-        idbPromise('products', 'get').then((products) => {
-            dispatch(updateProducts(products));
-        });
-    }
-    useEffect(() => {}, [data, loading, dispatch])
+    useEffect(() => {
+
+        if (data) {
+            dispatch(updateProducts(data.products));
+            data.products.forEach((product) => {
+                idbPromise('products', 'put', product);
+            });
+        } else if (!loading) {
+            idbPromise('products', 'get').then((products) => {
+                dispatch(updateProducts(products));
+            });
+        }
+    }, [data, loading, dispatch]);
+
+    const filterProducts = () => {
+        
+        if (!currentCategory) {
+            return products;
+        }
+
+        return products.filter((product) => product.category._id === currentCategory);
+    }; 
 
     return (
         <Box my={2}>
