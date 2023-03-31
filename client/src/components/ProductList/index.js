9b42faa4c4c1436f1716cf9c60d59ce1881e2731
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-31 15:30:23 -0700

    Added useEffect and dispatch to update product state

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -14,9 +14,21 @@ const ProductList = () => {
 
     // Grabs current category using a selector (Redux)
     const currentCategory = useSelector(selectCurrentCategory);
+    const dispatch = useDispatch();
     
     const { loading, data } = useQuery(QUERY_PRODUCTS);
-    
+
+    if (data) {
+        dispatch(updateProducts(data.products));
+        data.products.forEach((product) => {
+            idbPromise('products', 'put', product);
+        });
+    } else if (!loading) {
+        idbPromise('products', 'get').then((products) => {
+            dispatch(updateProducts(products));
+        });
+    }
+    useEffect(() => {}, [data, loading, dispatch])
 
     return (
         <Box my={2}>
