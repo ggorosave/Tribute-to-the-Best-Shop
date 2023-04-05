2d2885f714359f7677aeb8f550687835f265c95b
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-05 15:22:46 -0700

    Added basic framework for cart items

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,19 +1,59 @@
 import { useSelector, useDispatch } from "react-redux";
 import { removeFromCart, updateCartQuantity } from "../../utils/reducers/cartSlice";
 import { idbPromise } from "../../utils/helpers";
-import { 
-    Card, 
-    CardHeader, 
-    CardBody, 
+import {
+    Card,
+    CardHeader,
+    CardBody,
     CardFooter,
     Image,
-    Input
-} from '@chakra-ui/react'
+    Input,
+    Flex,
+    Text,
+    IconButton
+} from '@chakra-ui/react';
+import { FaTrashAlt } from "react-icons/fa";
 
 const CartItem = ({ item }) => {
 
     return (
         <Card borderRadius='none'>
+
+            {/* Image */}
+            <Image
+                src={`/images/${item.image}`}
+                alt={item.name}
+            />
+
+            <Flex w='full' flexDirection='space between'>
+
+                {/*  Item Name */}
+                <Text as='b'>{item.name}</Text>
+
+                {/* Item Price */}
+                <Text>${item.price}</Text>
+            </Flex>
+
+            {/* Label for input */}
+            <Text>Qty:</Text>
+            <Flex w='full' flexDirection='space between'>
+
+                {/* Input for Quantity */}
+                <Input 
+                    type="number"
+                    placeholder="1"
+                    value={item.purchaseQuantity}
+                    onChange={onChange}
+                />
+
+                {/* Delete Button */}
+                <IconButton 
+                    aria-label='Delete Item' 
+                    icon={<FaTrashAlt />}
+                    colorScheme="red"
+                />
+
+            </Flex>
 
         </Card>
     )
