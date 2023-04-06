3366354e7e25ea1fde3fbd94bf0d1ad8811be70a
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-06 08:38:13 -0700

    Added boilerplate code to Login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,19 @@
+import { useMutation } from "@apollo/client";
+import { Link as RoutLink } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
+import {
+    Link,
+    Box
+} from '@chakra-ui/react';
+
+const Login = (props) => {
+
+    return (
+        <Box>
+            
+        </Box>
+    )
+};
+
+export default Login;
