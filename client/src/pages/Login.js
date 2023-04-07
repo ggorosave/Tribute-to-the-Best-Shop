9224742ceb981fb70be14a427fca17b1e4294cf5
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-07 14:04:05 -0700

    Added handleFormSubmit function to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { Link as RoutLink } from "react-router-dom";
 import { LOGIN } from "../utils/mutations";
-import Auth from "../utils/auth";
 import {
     Link,
     Box
 } from '@chakra-ui/react';
+import Auth from "../utils/auth";
 
 const Login = (props) => {
+    const [formState, setFormState] = useState({
+        email: '',
+        password: ''
+    });
+    const [login, { error }] = useMutation(LOGIN); 
 
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const mutationResponse = await login({
+                variables: {
+                    email: formState.email,
+                    password: formState.password
+                },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    
     return (
         <Box>
             
