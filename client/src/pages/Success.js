4be2ee0a4c491bfc9dfe382d460bd9d0f68dcc64
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-10 10:38:21 -0700

    Added boilerplate code to Success Page

---
+++ client/src/pages/Success.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
+import {
+    Box,
+    Flex,
+    Heading
+} from "@chakra-ui/react";
+
+const Success = () => {
+
+    return (
+        <Box>
+            
+        </Box>
+    )
+};
+
+export default Success;
