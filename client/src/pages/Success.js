8172fe9a94f9f29d561b1a056ab9045015aa1177
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-10 10:46:44 -0700

    Added useEffect to save order to Success Page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -9,10 +9,33 @@ import {
 } from "@chakra-ui/react";
 
 const Success = () => {
+    const [addOrder] = useMutation(ADD_ORDER);
+
+    useEffect(() => {
+        const saveOrder = async () => {
+            const cart = await idbPromise('cart', 'get');
+            const products = cart.map((item) => item._id);
+
+            if (products.length) {
+                const { data } = await addOrder({ variables: { products } });
+                const productData = data.addOrder.products;
+
+                productData.forEach((item) => {
+                    idbPromise('cart', 'delete', item);
+                })
+            }
+
+            setTimeout(() => {
+                window.location.assign('/');
+            }, 3000)
+        }
+
+        saveOrder();
+    }, [addOrder]);
 
     return (
         <Box>
-            
+
         </Box>
     )
 };
