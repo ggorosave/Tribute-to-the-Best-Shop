9f707c410245b2650582ccf1932e62e4823d0278
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-13 09:40:52 -0700

    Added back button to error page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,16 +1,28 @@
 import { useRouteError } from "react-router-dom";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 import Jumbotron from "../components/Jumbotron"
 
 const NoMatch = () => {
     const error = useRouteError();
     console.error(error);
 
+    const sendHome = () => {
+        window.location.assign('/');
+    }
+
     return (
         <Box bg='quaternary.50' w='full' h='full' minHeight='100vh'>
             <Jumbotron>
                 <Heading as='h2' fontSize='2xl' mb={4}>Sorry, there seems to be an error.</Heading>
-                <Text fontSize='xl'>{error.statusText || error.message}</Text>
+                <Text fontSize='xl' mb={4}>{error.statusText || error.message}</Text>
+                <Button
+                    onClick={sendHome}
+                    leftIcon={<FaArrowAltCircleLeft />}
+                    colorScheme='quaternary'
+                    borderRadius='full'
+                    size='sm'
+                >Back</Button>
             </Jumbotron>
         </Box>
     )
