829c55e0c9394fb04a27cb19c94768747f87b300
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-13 09:48:01 -0700

    Added iconn

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,6 +1,7 @@
 import { useRouteError } from "react-router-dom";
-import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import { Box, Heading, Text, Button, Icon } from "@chakra-ui/react";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { MdWarningAmber } from "react-icons/md";
 import Jumbotron from "../components/Jumbotron"
 
 const NoMatch = () => {
@@ -14,6 +15,7 @@ const NoMatch = () => {
     return (
         <Box bg='quaternary.50' w='full' h='full' minHeight='100vh'>
             <Jumbotron>
+                <Icon as={MdWarningAmber} boxSize={14} mb={8} />
                 <Heading as='h2' fontSize='2xl' mb={4}>Sorry, there seems to be an error.</Heading>
                 <Text fontSize='xl' mb={4}>{error.statusText || error.message}</Text>
                 <Button
